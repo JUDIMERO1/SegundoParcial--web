7fdf3aea83599c84bc3fcf4fc2846eb430373c40
JUDIMERO1 <131618404+JUDIMERO1@users.noreply.github.com>
2024-05-31 10:25:17 -0500

    primer comit

---
+++ segundo-parcial-programacion-web-main/src/main/java/com/ufps/SegundoPrevioWebApplication.java
@@ -0,0 +1,13 @@
+package com.ufps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegundoPrevioWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegundoPrevioWebApplication.class, args);
+	}
+
+}
